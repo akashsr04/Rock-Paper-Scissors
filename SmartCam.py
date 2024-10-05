0a982d0982ce5a665efb8ee3759770188056d8ea
akashsr04 <akashscistory@gmail.com>
2024-10-05 09:43:55 +0530

    first commit

---
+++ SmartCam.py
@@ -0,0 +1,36 @@
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import tensorflow.keras as tf
+import cv2 
+
+
+#Program
+cam = cv2.VideoCapture(0)
+
+# Get the default frame width and height
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width,frame_height))
+
+while True:
+    ret, frame = cam.read()
+
+    # Write the frame to the output file
+    out.write(frame)
+
+    # Display the captured frame
+    cv2.imshow('SmartCam', frame)
+    print(frame)
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the capture and writer objects
+cam.release()
+out.release()
+cv2.destroyAllWindows()
