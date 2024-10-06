0a6a4522e7992b4c5bd21704f3aa4296f35e10f9
akashsr04 <165930157+akashsr04@users.noreply.github.com>
2024-10-06 20:01:51 +0530

    Update SmartCam.py

--- SmartCam.py
+++ SmartCam.py
@@ -5,32 +5,26 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow.keras as tf
 import cv2 
 
+import cv2
+import mediapipe
 
-#Program
-cam = cv2.VideoCapture(0)
-
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width,frame_height))
-
-while True:
-    ret, frame = cam.read()
-
-    # Write the frame to the output file
-    out.write(frame)
-
-    # Display the captured frame
-    cv2.imshow('SmartCam', frame)
-    print(frame)
-    # Press 'q' to exit the loop
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+  print('Camera Not Opened')
+else:
+  while True:
+    ret, frame=cap.read()
+    if not ret:
+      print("Could not Capture Frame")
+      break
+    else:
+      print(frame)
+      cv2.namedWindow('LiveStream',cv2.WINDOW_NORMAL)
+      cv2.imshow('LiveStream',frame)
     if cv2.waitKey(1) == ord('q'):
-        break
-
-# Release the capture and writer objects
-cam.release()
-out.release()
+      break
+print("Program execution finished")
+cap.release()
 cv2.destroyAllWindows()
+
+
